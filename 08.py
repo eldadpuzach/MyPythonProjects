0d709136a3f4b7ae703cce99579f00752cc14e78
eldadpuzach <eldadpuzach@gmail.com>
2018-06-16 17:16:19 +0300

    guess a number between 1 and 10

--- 08.py
+++ 08.py
@@ -5,4 +5,3 @@ target_num, guess_num = random.randint(1,10), 0
 while target_num != guess_num:
     guess_num = int(input('guess a number between 1 and 10, and keep guessing till you are right:'))
 print('game over')
-
