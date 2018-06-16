58bf5efb42e662707d622af5d9f3f2899a59101c
eldadpuzach <eldadpuzach@gmail.com>
2018-06-16 14:45:54 +0300

    print list items

---
+++ 08.2.py
@@ -0,0 +1,16 @@
+# print list items...
+
+pets = ['dog', 'cat', 'bird', 'rabbit', 'fish', 'neighbor']
+
+print(pets[0])
+print(pets[1])
+print(pets[2])
+print(pets[3])
+print(pets[4])
+print(pets[5], '\n')
+
+# or use a for loop
+
+for doodelegy in pets:
+    print(doodelegy)
+print('\n')
