3a961a7c265caf1369067d98e94564f01f1bde74
eldadpuzach <eldadpuzach@gmail.com>
2019-02-14 00:07:10 +0200

    Extract text regex

--- Extract_fromTxt/extract_text_config.py
+++ Extract_fromTxt/extract_text_config.py
@@ -1,6 +1,6 @@
 import re
 import os
-# import configparser
+import configparser
 # import codecs
 
 config = configparser.ConfigParser()
@@ -22,22 +22,24 @@ header = ['Document#', ',', 'Date', '\n']
 outfile = open('output.csv', 'w')
 outfile.write("".join(header))
 
-my_regex = r"351\+(.+?)\+009"
+my_regex = searchBGMvaluefrom,searchBGMvalueto,searchBGMvalueto
+print(my_regex)
 
-#iterates over each file in current dir
-for f in list_folder:
-    base_file, ext = os.path.splitext(f)
-    if ext != ".py":
-        if ext != ".csv":
-            if ext != ".ini":
-                lines = []  # Declare an empty list named "lines"
-                array = []
-
-                with open(f, 'r') as in_file:  # Opens each file in for loop for reading of text data.
-                    for line in in_file:  # For each line of text in in_file
-                        BGM = re.search(my_regex, line).group(1)
-                        DTM = re.search("137:(.+?):", line).group(1)
-                        array.extend([BGM, ',', DTM, "\n"])
-
-                        outfile = open('output.csv', 'a')
-                        outfile.write("".join(array))
+#
+# #iterates over each file in current dir
+# for f in list_folder:
+#     base_file, ext = os.path.splitext(f)
+#     if ext != ".py":
+#         if ext != ".csv":
+#             if ext != ".ini":
+#                 lines = []  # Declare an empty list named "lines"
+#                 array = []
+#
+#                 with open(f, 'r') as in_file:  # Opens each file in for loop for reading of text data.
+#                     for line in in_file:  # For each line of text in in_file
+#                         BGM = re.search(my_regex, line).group(1)
+#                         DTM = re.search("137:(.+?):", line).group(1)
+#                         array.extend([BGM, ',', DTM, "\n"])
+#
+#                         outfile = open('output.csv', 'a')
+#                         outfile.write("".join(array))
